0fc1a4af87019ae6be9c52da68d42698aba3ff9f
aitbensekko <aitbensekkour@gmail.com>
2026-01-18 14:24:55 +0100

    fix: update image loader to use correct static paths for export

--- image-loader.js
+++ image-loader.js
@@ -4,5 +4,10 @@ export default function myImageLoader({ src, width, quality }) {
     if (process.env.NODE_ENV === 'development') {
         return src
     }
-    return `/nextImageExportOptimizer/${src}?width=${width}&quality=${quality || 75}`
+    const lastSlashIndex = src.lastIndexOf('/')
+    const directory = src.substring(0, lastSlashIndex)
+    const fileNameFull = src.substring(lastSlashIndex + 1)
+    const fileNameWithoutExt = fileNameFull.replace(/\.[^/.]+$/, "")
+
+    return `${directory}/nextImageExportOptimizer/${fileNameWithoutExt}-opt-${width}.WEBP`
 }
